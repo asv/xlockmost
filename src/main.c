7a96d64a7d3cf2a367e6354083ea69f352fc49d9
Alexey A. Smirnov <alexey.smirnov@gmx.com>
2010-01-10 00:18:41 +0500

    small code refactoring

--- src/main.c
+++ src/main.c
@@ -10,24 +10,53 @@
 #include "utils.h"
 #include "pam_auth.h"
 
+#define MAX_PASSWD_LEN 256
+
 #define trace(format, args...) \
   fprintf (stderr, PACKAGE_NAME ": " format "\n", ##args);
+
+static Display *display = NULL;
+static Window window;
+
+static int doomsday = 1, pindex = 0;
+static char password[MAX_PASSWD_LEN + 1] = "";
+
+static void
+x11_init (int argc, char **argv);
+
+static void
+x11_cleanup (void);
+
+static void
+x11_do_main_loop (void);
+
+static void
+x11_break_main_loop (void);
+
+static void
+x11_handle_keypressed (XKeyPressedEvent *ev);
 
 int
 main (int argc, char **argv)
 {
-  Display *display;
-  Window window;
-  XSetWindowAttributes xswa;
-  XClassHint xch = {"xlockmost", "xlockmost"};
-
-  unsigned width, height, screen, depth;
-
-  int pindex = 0;
-  char password[1024];
-
   set_signal_handlers ();
   pam_auth_init ();
+
+  x11_init (argc, argv);
+  x11_do_main_loop ();
+  x11_cleanup ();
+
+  return EXIT_SUCCESS;
+}
+
+static void
+x11_init (int argc, char **argv)
+{
+  int screen, depth;
+  unsigned int width, height;
+
+  XSetWindowAttributes xswa;
+  XClassHint xch = {"xlockmost", "xlockmost"};
 
   display = XOpenDisplay (NULL);
 
@@ -52,7 +81,7 @@ main (int argc, char **argv)
                           CWOverrideRedirect | CWEventMask, &xswa);
 
   Xutf8SetWMProperties (display, window, "xlockmost", "xlockmost",
-                      argv, argc, NULL, NULL, &xch);
+                        argv, argc, NULL, NULL, &xch);
 
   XSetWindowBackground (display, window, BlackPixel (display, screen));
   XMapRaised (display, window);
@@ -61,88 +90,106 @@ main (int argc, char **argv)
                  GrabModeAsync, GrabModeAsync, CurrentTime);
 
   XFlush (display);
+}
 
-  FOREVER
+static void
+x11_cleanup (void)
+{
+  XUngrabKeyboard (display, CurrentTime);
+  XDestroyWindow (display, window);
+  XCloseDisplay (display);
+}
+
+static void
+x11_handle_keypressed (XKeyPressedEvent *ev)
+{
+  KeySym keysym;
+  char keystr[25];
+  int len, i;
+
+  len = XLookupString (ev, keystr, sizeof (keystr), &keysym, NULL);
+  switch (keysym)
     {
-      XEvent event;
-      XNextEvent (display, &event);
+    case XK_Delete:
+    case XK_KP_Delete:
+    case XK_BackSpace:
+      if (pindex > 0)
+        {
+          --pindex;
+        }
+      else
+        {
+          XBell (ev->display, 100);
+        }
+      break;
 
-      switch (event.type)
+    case XK_Linefeed:
+    case XK_KP_Enter:
+    case XK_Execute:
+    case XK_Return:
+      password[pindex] = '\0';
+      if (pam_auth_validate (password) == AUTH_OK)
+        {
+          x11_break_main_loop();
+          return;
+        }
+
+      XBell (display, 100);
+    case XK_Escape:
+    case XK_Clear:
+      pindex = 0;
+      break;
+
+    default:
+      if (len == 0)
+        {
+          if (IsModifierKey (keysym) != True)
+            {
+              XBell (ev->display, 100);
+            }
+
+          return;
+        }
+
+      for (i = 0; i < len; ++i)
+        {
+          password[pindex++] = keystr[i];
+          if (pindex == MAX_PASSWD_LEN)
+            {
+              pindex = 0;
+            }
+        }
+      break;
+    }
+}
+
+static void
+x11_do_main_loop (void)
+{
+  XEvent ev;
+  while (doomsday)
+    {
+      XNextEvent (display, &ev);
+
+      switch (ev.type)
         {
         case KeyPress:
-            {
-              KeySym keysym;
-              int len, i;
-              char keystr[25];
-
-              len = XLookupString (&event.xkey, keystr,
-                                   sizeof (keystr), &keysym, NULL);
-              switch (keysym)
-                {
-                case XK_Delete:
-                case XK_KP_Delete:
-                case XK_BackSpace:
-                  if (pindex > 0)
-                    {
-                      --pindex;
-                    }
-                  else
-                    {
-                      XBell (display, 100);
-                    }
-                  break;
-
-                case XK_Linefeed:
-                case XK_KP_Enter:
-                case XK_Execute:
-                case XK_Return:
-                  password[pindex] = '\0';
-                  if (pam_auth_validate (password) == AUTH_OK)
-                    {
-                      goto exit_success;
-                    }
-
-                  XBell (display, 100);
-                case XK_Escape:
-                case XK_Clear:
-                  pindex = 0;
-                  break;
-                default:
-                  if (len == 0)
-                    {
-                      if (IsModifierKey (keysym) != True)
-                        {
-                          XBell (display, 100);
-                        }
-                      continue;
-                    }
-
-                  for (i = 0; i < len; ++i)
-                    {
-                      password[pindex++] = keystr[i];
-                      if (pindex == sizeof (password))
-                        {
-                          pindex = 0;
-                        }
-                    }
-                  break;
-                }
-            }
+          x11_handle_keypressed (&ev.xkey);
           break;
         case KeyRelease:
           break;
         case Expose:
           break;
         default:
-          trace ("unknown event type %d", event.type);
+          trace ("unknown event type %d", ev.type);
           break;
         }
     }
-
-exit_success:
-  XUngrabKeyboard (display, CurrentTime);
-  XDestroyWindow (display, window);
-  XCloseDisplay (display);
-
-  return EXIT_SUCCESS;
 }
+
+static void
+x11_break_main_loop (void)
+{
+  doomsday = 0;
+}
+
